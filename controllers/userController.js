c819eaeb6fcf0e7e63ccebdd8012d5209db631d7
Jaydev <pawarjaydev96@gmail.com>
2023-05-24 09:55:48 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,15 @@
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 exports.register = async (req, res) => {
     try {
-        const { password } = req.body
-
+        const { password, email } = req.body
+        const found = await User.findOne({ email })
+        if (found) {
+            return res.json({
+                message: "Email already exist"
+            })
+        }
         const hashPass = await bcrypt.hash(password, 10)
         const result = await User.create({
             ...req.body,
@@ -21,13 +27,53 @@ exports.register = async (req, res) => {
 }
 exports.fetchUsers = async (req, res) => {
     try {
-        const result = await User.find(req.body)
+        const token = req.headers.authorization
+        if (!token) {
+
+            res.json({ message: "Provide Token" })
+        }
+        jwt.verify(token, process.env.JWT_KEY)
+
+        const result = await User.find()
         res.json({
             message: "user fetch successs",
             result
         })
     } catch (error) {
         res.json({ message: "something went wrong", error })
+    }
+
+}
+exports.login = async (req, res) => {
+    try {
+        //email exist
+        const { email, password } = req.body
+        const result = await User.findOne({ email })
+
+        if (!result) {
+            return res.json({ message: "email is not registered with us" })
+        }
+        const match = await bcrypt.compare(password, result.password)
+        if (!match) {
+            return res.json({ message: "password do not match" })
+        }
+        const token = jwt.sign({ name: "john" }, process.env.JWT_KEY)
+        res.json({ message: "login success", token })
+    } catch (error) {
+        res.json({ message: "something went wrong" + error })
+    }
+
+}
+exports.destroy = async (req, res) => {
+    try {
+
+
+        await User.deleteMany()
+        res.json({
+            message: "user destroy successs",
+        })
+    } catch (error) {
+        res.json({ message: "something went wrong" + error })
     }
 
 }
